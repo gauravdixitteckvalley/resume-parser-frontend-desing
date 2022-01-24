aad58baf603308bc7821045d01899aeea9290923
Gaurav Dixit <gauravdixit@teckvalley.com>
2022-01-24 21:42:27 +0530

    add code to logo issue

--- src/pages/layouts/Header/index.js
+++ src/pages/layouts/Header/index.js
@@ -69,7 +69,7 @@ const Header = (props) => {
         <>
             <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row sidebar-icon-only">
                 <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                    <a className="navbar-brand brand-logo" href="index.html"><img src="/logo.png" alt="logo" /></a> 
+                    <a className="navbar-brand brand-logo" href="index.html"><img src="/logo.PNG" alt="logo" /></a> 
                     <a className="navbar-brand brand-logo-mini" href="index.html"><img src="/mobile_logo.png" alt="logo" /></a> 
                 </div>
                 <div className="navbar-menu-wrapper d-flex align-items-stretch">
