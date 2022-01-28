b65b7bce2492f8b4d7792d0010f3da3f266de3fc
Kuldeep <kuldeeprawat@virtualemployee.com>
2022-01-28 12:04:34 +0530

    candidate details

--- src/containers/Resume/CandidateDetails.js
+++ src/containers/Resume/CandidateDetails.js
@@ -109,13 +109,9 @@ const CandidateDetails = (props) => {
     const { user } = loggedUser;
     // const blocking = false;
 
-    return (
-        <Fragment>
-            <BlockUI blocking={blocking} />
-            <div className="page-header">
-              <h3 className="page-title"> Update Details</h3>
-            </div>
-            
+    const candidateEditPage = (isCandidateLogin) => {
+        if(isCandidateLogin === false){
+            return (
             <div className="row">
                 <div className="col-lg-12 grid-margin stretch-card">
                     <div className="card">
@@ -245,204 +241,23 @@ const CandidateDetails = (props) => {
                     </div>
                   </div>
             </div>
+            )
+        } else {
+            return (
+                <p>Admin Login</p>
+            )
+        }
+    }
 
+    return (
+        <Fragment>
+            <BlockUI blocking={blocking} />
+            <div className="page-header">
+              <h3 className="page-title"> Update Details</h3>
+            </div>
 
-            {/* <form onSubmit={(event) => _handleSubmit(event)}>
-                <div className="row clearfix mb-3">
-                    <div className="col-lg-12 col-md-12 col-sm-12">
-                        <div className="row clearfix">
-                        <div className="col-md-6 mb-3"> <b className="required">Name</b>
-                                <div className="form-group">
-                                    <input type="text" name="name" className="form-control"
-                                            placeholder="Martin Philipes" 
-                                            value={fields.name || ''} 
-                                            onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.name}</div>        
-                                </div>
-                            </div>
-
-                            <div className="col-md-6 mb-3"> <b className="required">Email</b>
-                                <div className="form-group">
-                                    <input type="email" name="email" className="form-control"
-                                            placeholder="dummy@faker.com" 
-                                            value={fields.email || ''}
-                                            onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.email}</div>        
-                                </div>
-                            </div>
-
-                            <div className="col-md-6 mb-3"> <b className="required">Phone</b>
-                                <div className="form-group">
-                                    <input type="text" name="phone" className="form-control" 
-                                            placeholder="+91 1232222124"
-                                            value={fields.phone || ''}
-                                            onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.phone}</div>        
-                                </div>
-                            </div>
-
-                            <div className="col-md-6 mb-3"> <b className="required">D.O.B</b>
-                                <div className="form-group">
-                                    <input className="form-control" type="date" name="dob" 
-                                     value={fields.dob || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.dob}</div>
-                                </div>
-                            </div>
-
-                            <div className="col-md-12 mb-3"> Address
-                                <div className="form-group">
-                                    {/* <input className="form-control" type="text" name="location" 
-                                     value={fields.location || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    /> 
-                                    <textarea className="form-control" name="location" 
-                                     value={fields.location || ''} 
-                                        onChange={(event) => _handleChange(event)}></textarea>
-                                    <div className="errorMsg">{errors.location}</div>
-                                </div>
-                            </div>
-                                                        <div className="col-md-3 mb-3"> <b className="required">Country</b>
-                                <div className="form-group">
-                                    
-                                    <select name="country" className="form-control" value={fields.country || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    >
-                                        <option value="">Select Country</option>
-                                        {countryList?.map((country, index) => (
-                                            <option key={index} value={country._id}>{country.name}</option>
-                                        ))}
-                                    </select>
-                                    <div className="errorMsg">{errors.country}</div>        
-                                </div>
-                            </div>
-                            <div className="col-md-3 mb-3"> <b className="required">State</b>
-                                <div className="form-group">
-                                
-                                    <select name="state" className="form-control" value={fields.state || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    >
-                                        <option value="">Select State</option>
-                                        {stateList?.map((state, index) => (
-                                            <option key={index} value={state._id}>{state.name}</option>
-                                        ))}
-                                    </select>
-                                    <div className="errorMsg">{errors.state}</div>        
-                                </div>
-                            </div>
-                            <div className="col-md-3 mb-3"> <b className="required">City</b>
-                                <div className="form-group">
-                                   
-                                    <input className="form-control" type="text" name="place" 
-                                     value={fields.place || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                    {/* <select name="city" className="form-control" value={fields.city || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    >
-                                        <option value="">Select City</option>
-                                        {cityList?.map((city, index) => (
-                                            <option key={index} value={city._id}>{city.name}</option>
-                                        ))}
-                                    </select> 
-                                    <div className="errorMsg">{errors.city}</div>        
-                                </div>
-                            </div>
-                            <div className="col-md-3 mb-3"> <b className="required">Zip</b>
-                                <div className="form-group">
-                                    <input type="text" name="zip" className="form-control"
-                                            placeholder="Zip" 
-                                            value={fields.zip || ''} 
-                                            onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.zip}</div>        
-                                </div>
-                            </div> 
-
-                            <div className="col-md-6 mb-3"> <b className="required">Company Name</b>
-                                <div className="form-group">
-                                    <input type="text" name="workExperience" className="form-control"
-                                            placeholder="Company name" 
-                                            value={fields.workExperience || ''} 
-                                            onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.workExperience}</div>        
-                                </div>
-                            </div>
-
-                            <div className="col-md-6 mb-3"> <b className="required">Skills</b>
-                                <div className="form-group">
-                                    <input type="text" name="skills" className="form-control" 
-                                            placeholder="HTML,CSS,PHP,.NET,JAVASCRIPT"
-                                            value={fields.skills || ''}
-                                            onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.skills}</div>        
-                                </div>
-                            </div>
-                            
-                            <div className="col-md-6 mb-3"> <b className="required">Total Experience</b>
-                                <div className="form-group">
-                                    <input className="form-control" type="text" name="exp" 
-                                     value={fields.exp || ''}  
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.exp}</div>
-                                </div>
-                            </div>
-                            <div className="col-md-6 mb-3"> <b className="required">Designation</b>
-                                <div className="form-group">
-                                    <input className="form-control" type="text" name="designation" 
-                                     value={fields.designation || ''}  
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.designation}</div>
-                                </div>
-                            </div>
-                            <div className="col-md-6 mb-3"> <b className="required">Current CTC</b>
-                                <div className="form-group">
-                                    <input className="form-control" type="text" name="current_ctc" 
-                                     value={fields.current_ctc || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.current_ctc}</div>
-                                </div>
-                            </div>
-                            <div className="col-md-6 mb-3"> Expected CTC
-                                <div className="form-group">
-                                    <input className="form-control" type="text" name="expected_ctc" 
-                                     value={fields.expected_ctc || ''} 
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                    <div className="errorMsg">{errors.expected_ctc}</div>
-                                </div>
-                            </div>
-                            <div className="col-md-6 mb-3">Other Information
-                                <div className="form-group">
-                                    <textarea className="form-control" name="resume_label" cols="30" rows="3" 
-                                     value={fields.resume_label || ''} 
-                                    onChange={(event) => _handleChange(event)}></textarea>
-                                    <div className="errorMsg">{errors.resume_label}</div>
-                                </div>
-                            </div>
-                            <div className="col-md-6 mb-3"> <b className="required">Resume</b>
-                                <div className="form-group">
-                                    <input className="fileInput" type="file" name="document" 
-                                        onChange={(event) => _handleChange(event)} 
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    
-                <button type="submit" className="btn btn-success">Submit</button>
-                <button className="btn btn-danger ms-2" onClick={_handleCancelForm}>Cancel</button>
-            </form> */}
+            {candidateEditPage(user.isCandidateLogin)}
+                
         </Fragment>
     )
 }
