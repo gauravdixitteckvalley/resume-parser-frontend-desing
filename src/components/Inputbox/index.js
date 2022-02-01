5c02a7c05a7f96d42b6b5a73eba865d497146902
ShubhamVirtual <shubhamghavari@virtualemployee.com>
2022-02-01 14:07:12 +0530

    input componant

--- src/components/Inputbox/index.js
+++ src/components/Inputbox/index.js
@@ -1,30 +1,17 @@
 import React from "react";
 
-const Inputbox = ({labelStatus,labelValue,labelClass,labelFor, id, type, name, handleClick, className, value, minLength, maxLength,placeholder,readonly }) => {
-  return (
-    <>
-        {labelStatus ? 
-            <label className={labelClass} htmlFor={labelFor}>
-                {labelValue}
-            </label>
-            : ""
-        }
-        
-        <input
-            id={id ? id : ""}
-            name={name ? name : ""}
-            type={type}
-            onChange={handleClick}
-            className={className ? className : ""}
-            value={value ? value : ""}
-            minLength={minLength ? minLength :""}
-            maxLength={maxLength ? maxLength :""}
-            placeholder={placeholder ? placeholder : ""}
-            readOnly={readonly?readonly:false}
-        />
-    </>
+const InputBox = ({InputValue, handleClick})=>{
+    // const { InputValue } = props;
     
-  );
-};
+    return( 
+        <input type="text"
+            className="form-control mb-2 mr-sm-2"
+            value = {InputValue || ''}
+            onChange={handleClick}
+            name="skill"
+        />
+        
+    )
+}
 
-export default Inputbox;
+export default InputBox;
